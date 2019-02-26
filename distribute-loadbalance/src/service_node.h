a4cbc2705ab09da73eaf7f971fb0024107e456be
perrynzhou <perrynzhou@gmail.com>
2019-02-26 14:24:34 +0800

    format conf file

--- distribute-loadbalance/src/service_node.h
+++ distribute-loadbalance/src/service_node.h
@@ -10,7 +10,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <pthread.h>
-#include <functional>
 typedef int (*readCb)(int cfd);
 typedef int (*writeCb)(int cfd, void *ptr, size_t len);
 typedef int (*exceptionCb)(int cfd, const char *msg,size_t len);
