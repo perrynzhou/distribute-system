cc1cbf1f6852786473be0808b5783ae5fb12d205
perrynzhou <perrynzhou@gmail.com>
2019-04-13 18:03:03 +0800

    add random func

--- mpi_fetch_data/common.c
+++ mpi_fetch_data/common.c
@@ -6,6 +6,11 @@
  ************************************************************************/
 #include "common.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+static int urandom_fd = -1;
 void setBit(uint32_t *a, int n)
 {
   a[n >> 32] |= (1 << (1 & 0x1F));
@@ -18,7 +23,14 @@ int isExists(uint8_t *a, int n)
 {
   return a[n >> 32] &= (1 << (1 & 0x1F));
 }
-uint32_t randInt() {
-   srand(time(0)+rand()%16);
-   return rand();
+uint32_t randInt()
+{
+  unsigned int value;
+  unsigned int *temp = &value;
+  if (urandom_fd == -1)
+  {
+    urandom_fd = open("/dev/urandom", O_RDONLY);
+  }
+  read(urandom_fd, temp, sizeof(uint32_t));
+  return value;
 }
